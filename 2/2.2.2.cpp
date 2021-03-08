53f46c8be0f1747f06bdea8263c4ac1fd65bcada
DmitryTD <79697994+DmitryTD@users.noreply.github.com>
2021-03-08 21:58:32 +0300

    Add files via upload

---
+++ 2/2.2.2.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+	int val3 = 2, n, val2 = 1, temp;
+	cin >> n;
+	if (n > 3) {
+		for (int i = 0; i < n - 3; i++) {
+			temp = val3 % 10;
+			val3 = (val3 + val2) % 10;
+			val2 = temp;
+		}
+	}
+	else if (n == 1 || n == 2) {
+		val3 = 1;
+	}
+
+	cout << val3 << endl;
+}
